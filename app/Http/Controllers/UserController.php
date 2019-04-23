51b196276c2d476476a6007dfe96f6dfd3e13fdb
Reo O <rokumura@nexseed.net>
2019-04-23 14:42:38 +0800

    Fixed index method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view('users.mypage', compact(user));
+        return view('users.mypage', compact('user'));
     }
 
     public function create()
